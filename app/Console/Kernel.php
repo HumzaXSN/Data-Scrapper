7d5c83fd9f3781078f7d8eb9e97c1348688fa35c
Muhammad Humza Riaz <humza@ashlarglobal.com>
2022-05-20 15:20:08 +0500

    Schedule Time Updated to Midnight.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -33,7 +33,7 @@ class Kernel extends ConsoleKernel
                 $getData[0]->location,
                 $getData[0]->limit,
                 $getData[0]->id
-            ])->everyMinute()->timezone('Asia/Karachi');
+            ])->daily()->timezone('Asia/Karachi');
         }
     }
 

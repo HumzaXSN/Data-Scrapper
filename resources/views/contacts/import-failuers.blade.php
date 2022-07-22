f116156a0417875a43a03fca8b7d8d5cb50c03af
HumzaXSN <37052449+HumzaXSN@users.noreply.github.com>
2022-07-22 10:46:34 +0500

    removed space

--- resources/views/contacts/import-failuers.blade.php
+++ resources/views/contacts/import-failuers.blade.php
@@ -67,7 +67,6 @@
                                             </div>
                                             @endforeach
                                         @endif
-
                                     </div>
 
                                     <!-- Modal footer -->
